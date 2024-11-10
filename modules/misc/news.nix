a7f56b3181329368e9fad9856a5536d3bf89bc30
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-11-10 23:35:15 +0100

    wayfire: add module

--- modules/misc/news.nix
+++ modules/misc/news.nix
@@ -1827,6 +1827,21 @@ in {
           as systemd services.
         '';
       }
+
+      {
+        time = "2024-11-13T00:12:00+00:00";
+        condition = hostPlatform.isLinux;
+        message = ''
+          A new module is available: 'wayland.windowManager.wayfire'.
+
+          Wayfire is a 3D Wayland compositor, inspired by Compiz and based on
+          wlroots. It aims to create a customizable, extendable and lightweight
+          environment without sacrificing its appearance.
+
+          This Home Manager module allows you to configure both wayfire itself,
+          as well as wf-shell.
+        '';
+      }
     ];
   };
 }
